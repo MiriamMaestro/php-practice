cf19efcecc8a19dc165804b23a4fd2f3e9334785
Miriam Maestro <yosoymairim@gmail.com>
2021-01-18 16:49:06 +0100

    enter

--- js/practice1.js
+++ js/practice1.js
@@ -3,17 +3,12 @@ $(document).ready(() => {
     const $btnRun2 = $('#btnRun2');
     const $btnRun3 = $('#btnRun3');
 $btnRun.on('click', () => {
-    //var selectedCountry = $("#selCountry :selected").val();
-   //var selectedLatitud = $("#selCountry :selected").attr("data-latitud").val();
-  // var selectedAltitud = $("#selCountry :selected").attr("data-altitud").val();
-  
+ 
    $.ajax({
     url: "php/TimeZone.php",
     type: 'POST',
     dataType: 'json',
     data: {
-        //latitud: $("#selCountry :selected").attr("data-latitud").val(),
-        //altitud: $("#selCountry :selected").attr("data-altitud").val()
         latitud: $("#selCountry").find('option:selected').attr("data-latitud"),
         altitud: $("#selCountry").find('option:selected').attr("data-altitud")
     },
@@ -25,17 +20,13 @@ $btnRun.on('click', () => {
 
             $('#sunrise').html('Sunrise: ' + result['data']['sunrise']);
             $('#sunset').html('Sunset: '+result['data']['sunset']);
-            //$('#txtLanguages').html(result['data'][0]['languages']);
-            //$('#txtPopulation').html(result['data'][0]['population']);
-            //$('#txtArea').html(result['data'][0]['areaInSqKm']);
 
         }
       
     
     },
     error: function(jqXHR, textStatus, errorThrown) {
-        // your error code
-        console.log("no quena");
+        console.log("it´s not working");
     }
 }); 
 
@@ -59,14 +50,12 @@ $btnRun2.on('click', () => {
 
             $('#ocean').html('Ocean name: ' + result['data']['name']);
             $('#sunset').html(' ');
-            
-
+           
         } 
 
     },
     error: function(jqXHR, textStatus, errorThrown) {
         console.log('No funcion');
-       // $('#sunset').html('No hay oceano');
     }
 }); 
 });
@@ -99,7 +88,7 @@ $btnRun3.on('click', () => {
      },
      error: function(jqXHR, textStatus, errorThrown) {
          console.log('No funcion');
-        // $('#sunset').html('No hay oceano');
+
      }
  }); 
 
